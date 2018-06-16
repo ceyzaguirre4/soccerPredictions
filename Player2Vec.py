79a294887205023fc6e67d7327dc7e6d5c5252a0
Cristobal Eyzaguirre <ceyzaguirre4@uc.cl>
2018-06-16 16:04:12 -0400

    solved bug wip kaggle DB

--- Player2Vec.py
+++ Player2Vec.py
@@ -11,8 +11,8 @@ import json
 # In[11]:
 
 
-pos2index = {'LF': 16, 'RWB': 2, 'DM': 7, 'LB': 5, 'CM': 12, 'CB': 6, 'AM': 13, 'LM': 10, 'SW': 1, 'CF': 14, 'ST': 17, 'LW': 9, 'RF': 15, 'RW': 8, 'RB': 4, 'LWB': 3, 'RM': 11, 'GK': 0}
-foot2index = {"Right":0, "left":1}
+pos2index = {'CAM':19, 'CDM':18, 'LF': 16, 'RWB': 2, 'DM': 7, 'LB': 5, 'CM': 12, 'CB': 6, 'AM': 13, 'LM': 10, 'SW': 1, 'CF': 14, 'ST': 17, 'LW': 9, 'RF': 15, 'RW': 8, 'RB': 4, 'LWB': 3, 'RM': 11, 'GK': 0}
+foot2index = {"Right":0, "Left":1}
 
 
 # In[12]:
@@ -29,9 +29,8 @@ def player2vec(id):
         return
     resp = requests.get('http://www.easports.com/fifa/ultimate-team/api/fut/item?jsonParamObject={"id":"' + str(id) + '"}')
     obj = json.loads(resp.text)
-    try:
+    if obj["items"]:                           # if player data exists in sofifa
         player = obj["items"][0]
-        print(player["name"])
         return (pos2index[player["position"]],
             int(player["birthdate"].split("-", 1)[0]),
             foot2index[player["foot"]],
@@ -74,8 +73,6 @@ def player2vec(id):
             player["volleys"],
             player["weakFoot"],
             player["rating"])
-    except:
-        return
     
 
 
